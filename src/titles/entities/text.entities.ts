4fe9d1e5aefa0cf4f7eb6c32e60b819e36e66e70
artmusicrr <artmusic_rr@msn.com>
2024-05-25 20:34:45 -0300

    correção do update de table_title

--- src/titles/entities/text.entities.ts
+++ src/titles/entities/text.entities.ts
@@ -20,53 +20,42 @@ export class TitleRepository {
   }
 
   async updateByIdText(id_text: number, request: any): Promise<any> {
-    console.log('request ==>', request.id_text);
+    //console.log('request ==>', request.id_text);
     const { text } = request;
-    const query = `UPDATE public.text_title SET text=$1 WHERE id_text = $2 RETURNING *`;
+    const query = `UPDATE public.text_title SET text = $1 WHERE id_text = $2 RETURNING *;`;
     const values = [text, id_text];
-    console.log('VALUES ===>>', values, id_text);
+    //console.log('VALUES ===>>', values, id_text);
     const result = await this.db.query(query, values);
     return result.rows[0];
   }
 
   async updateByIdTitle(id_text: number, request: any): Promise<any> {
-    console.log('request ==>', request.id_text);
+    //console.log('request ==>', request.id_text);
     const { title } = request;
-    const query = `UPDATE public.text_title SET title=$1 WHERE id_text = $2 RETURNING *`;
+    const query = `UPDATE public.text_title SET title =$1 WHERE id_text = $2 RETURNING *;`;
     const values = [title, id_text];
-    console.log('VALUES ===>>', values, id_text);
+    //console.log('VALUES ===>>', values, id_text);
     const result = await this.db.query(query, values);
     return result.rows[0];
   }
 
   async updateByIdSubTitle(id_text: number, request: any): Promise<any> {
-    console.log('request ==>', request.id_text);
+    //console.log('request ==>', request.id_text);
     const { sub_title } = request;
-    const query = `UPDATE public.text_title SET sub_title=$1 WHERE id_text = $2 RETURNING *`;
+    const query = `UPDATE public.text_title SET sub_title = $1 WHERE id_text = $2 RETURNING *;`;
     const values = [sub_title, id_text];
-    console.log('VALUES ===>>', values, id_text);
+    //console.log('VALUES ===>>', values, id_text);
     const result = await this.db.query(query, values);
     return result.rows[0];
   }
 
   async updateByIdAnyText(id_text: number, request: any): Promise<any> {
-    console.log('request ==>', request.id_text);
+    // console.log('request ==>', request.id_text);
     const { any_text } = request;
-    const query = `UPDATE public.text_title SET any_text=$1 WHERE id_text = $2 RETURNING *`;
+    const query = `UPDATE public.text_title SET any_text = $1 WHERE id_text = $2 RETURNING *;`;
     const values = [any_text, id_text];
-    console.log('VALUES ===>>', values, id_text);
+    // console.log('VALUES ===>>', values, id_text);
     const result = await this.db.query(query, values);
     return result.rows[0];
   }
-
-  //   async findByName(name: string): Promise<any> {
-  //     try {
-  //       const query = `
-  //       SELECT * FROM public.users where name = '${name}';`;
-  //       const result = await this.db.query(query);
-  //       return result.rows[0];
-  //     } catch (error) {
-  //       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-  //     }
-  //   }
 }
