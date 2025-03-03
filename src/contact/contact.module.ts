0badd1054f766f21299850ec1b45ae46715ce241
artmusicrr <artmusic_rr@msn.com>
2025-03-02 23:03:17 -0300

    remoção de logs

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { ContactController } from './contact.controller';
 import { DatabaseModule } from '../database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    ConfigModule // Since ConfigModule.forRoot({ isGlobal: true }) is in AppModule, we just need to import ConfigModule here
+  ],
   controllers: [ContactController],
   providers: [ContactService],
 })
